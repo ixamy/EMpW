cea2d9353c8ba810d46b81668117e87ae067d72d
Maximiliano <oscaralfonso93@hotmail.com>
2021-05-26 20:38:21 -0300

    Add files via upload

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,13 +1,16 @@
-
-var arg = true;
+BUSSINES='Avlis'
+OPEN='09:00'
+CLOSE='19:00'
+DAY_FROM='Lunes'
+DAY_TO='Sábados'
 
 function devuelveHora(){
 	var fecha = new Date();
   if(fecha.getHours() <= 12){
-    return "Buen día";
+    saludo = "Buen día";
   } else if(fecha.getHours() > 12){
-    return "Buenas tardes";
-  }
+    saludo = "Buenas tardes";
+  } return saludo;
 }
 
 function areaCode(){
@@ -19,7 +22,18 @@ function areaCode(){
     number = "598" + number;
   }
   document.getElementById("phone").value = number;
-  //alert("ANOTAR EN EL SISTEMA QUE SE AVISO");
+}
+
+function argentina(){
+  if(document.getElementById("flag").checked == true){
+    document.getElementById("phone").maxLength = 13;
+    document.getElementById("phone").pattern = ".{13,}";
+    document.getElementById("phone").placeholder = "54911 1234-5678";
+  } else{
+    document.getElementById("phone").maxLength = 9;
+    document.getElementById("phone").pattern = ".{9,}";
+    document.getElementById("phone").placeholder = "Celular"
+  }
 }
 
 function nombre(){
@@ -30,38 +44,37 @@ function nombre(){
   }
 }
 
-function equipoListo(){
-  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde Avlis para avisarle que su equipo esta listo. ¿Cuando podria pasar?"
-}
-function sinSolucion(){
-  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde Avlis para avisarle que lamentablemente su equipo no tuvo reparación, recuerde que conservamos equipos en este estado por un maximo de 60 días, luego de ese tiempo se descartan. Le esperamos de Lunes a Sábados de 09:00 a 20:00."
-}
-function llegoRepuesto(){
-  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde Avlis para avisarle que ya nos llego el repuesto para su equipo, le esperamos de Lunes a Sábados de 09:00 a 20:00."
-}
-function desbloqueo(){
-  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde Avlis para avisarle que su equipo tiene código de desbloqueo y el técnico lo precisa para continuar con la reparación, nos lo puede proporcionar por esta misma vía."
-}
-
-function argentina(){
-  if(arg === true){
-    document.getElementById("phone").maxLength = 13;
-    document.getElementById("phone").pattern = ".{13,}";
-    document.getElementById("phone").placeholder = "54911 1234-5678";
-    arg = false;
-  } else{
-    document.getElementById("phone").maxLength = 9;
-    document.getElementById("phone").pattern = ".{9,}";
-    document.getElementById("phone").placeholder = "Celular"
-    arg = true;
+function formato(){
+  nombreArray = document.getElementById("nombre").value.split(" ");
+  if(nombreArray.length<2){
+	  document.getElementById("nombre").value = nombreArray[0][0].toUpperCase() + nombreArray[0].slice(1).toLowerCase();
+  } else {
+	  for(var i=0;i<nombreArray.length;i++){
+		  nombreArray[i] = nombreArray[i][0].toUpperCase() + nombreArray[i].slice(1).toLowerCase();
+	  } document.getElementById("nombre").value = nombreArray.join(" ");
   }
 }
 
+function equipoListo(){
+  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde " + BUSSINES + " para avisarle que su equipo esta listo. ¿Cuándo podría pasar?"
+}
+function sinSolucion(){
+  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde " + BUSSINES + " para avisarle que lamentablemente su equipo no tuvo reparación, recuerde que conservamos equipos en este estado por un máximo de 60 días, luego de ese tiempo se descartan. Le esperamos de " + DAY_FROM + " a " + DAY_TO + " de " + OPEN + " a " + CLOSE + "."
+}
+function llegoRepuesto(){
+  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde " + BUSSINES + " para avisarle que ya nos llego el repuesto para su equipo, le esperamos de " + DAY_FROM + " a " + DAY_TO + " de " + OPEN + " a " + CLOSE + "."
+}
+function desbloqueo(){
+  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde " + BUSSINES + " para avisarle que su equipo tiene código de desbloqueo y el técnico lo precisa para continuar con la reparación, nos lo puede proporcionar por esta misma vía."
+}
+function armado(){
+  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde " + BUSSINES + " para avisarle que su equipo esta armado para ser retirado."
+}
 function presupuesto(){
   if(document.getElementById("precio").value !== ""){
-    document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde Avlis para avisarle que el presupuesto para su equipo es de " + document.getElementById("precio").value + ", ¿Lo acepta? Recuerde que el mismo tiene una validez de 30 días.";
+    document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde " + BUSSINES + " para avisarle que el presupuesto para su equipo es de " + document.getElementById("precio").value + ", ¿Lo acepta? Recuerde que el mismo tiene una validez de 30 días.";
   } else{
-      document.getElementById("precio").placeholder = "INGRESAR PRECIO"
+      document.getElementById("precio").placeholder = "Ingresar monto"
       document.getElementById("precio").style.backgroundColor = "yellow";
       document.getElementById("radio_presupuesto").checked = false;
   }
@@ -70,13 +83,4 @@ function presupuesto(){
 function estadoPresupuesto(){
   document.getElementById("precio").placeholder = "Presupuesto";
   document.getElementById("precio").style.backgroundColor = "white";
-}
-
-function formato(){
-  document.getElementById("nombre").value = document.getElementById("nombre").value.replace(/\s/g, '')
-  document.getElementById("nombre").value = document.getElementById("nombre").value[0].toUpperCase() + document.getElementById("nombre").value.slice(1).toLowerCase();
-}
-
-function armado(){
-  document.getElementById("textarea").value = devuelveHora() + nombre() + ", le escribo desde Avlis para avisarle que su equipo esta armado para ser retirado."
 }
